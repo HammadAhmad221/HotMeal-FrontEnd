7efcb1694911f9fee3c4c4dd6b40696267b4a1c4
Sajjad <sajjadahmad3419@gmail.com>
2023-06-08 23:44:49 -0700

    design-done

---
+++ src/app/restaurant-page/restaurant-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-page',
+  templateUrl: './restaurant-page.component.html',
+  styleUrls: ['./restaurant-page.component.scss']
+})
+export class RestaurantPageComponent {
+
+}
