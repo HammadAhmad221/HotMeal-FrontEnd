bbe9bc07034f8fffd46b4b590fede62134b4ea86
Hammad <hammadchahal123@gmail.com>
2023-08-19 18:12:13 +0500

    Loader and resturant page done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { getAllResturants } from '../store/resturant/resturant.selector';
-// import { fetchAllResturants } from '../store/resturant/resturant.action';
 import { IGetResturantRequest } from '../models/request/getResturant.request';
 import * as ResturantActions from '../store/resturant/resturant.action';
 import { Router } from '@angular/router';
@@ -17,7 +16,7 @@ export class HomeComponent implements OnInit {
   allRestuarants:any[]=[];
   featuredResturants: any[] = [];
   recommendedResturants:any[]=[];
-  isLoading: boolean = false;
+  isLoading: boolean = true;
   constructor(private store: Store,private router:Router) {}
   onSelectRestaurant(id:string){
     this.router.navigate([id])
@@ -30,23 +29,19 @@ export class HomeComponent implements OnInit {
          featured: false,
          plan: 0,
    }
-
-   this.isLoading = true;
-
   this.store.select(getAllResturants).subscribe((data) => {
      this.isLoading=true;
     if (data != null && data.length > 0) {
-      // this.isLoading=true;
       this.allRestuarants = data;
       this.featuredResturants = data.filter((resturant) => resturant.featured === true);
       this.recommendedResturants=data.filter((resturant)=>resturant.rating>=4);
-      // this.isLoading=false;
     } else {
-      // this.isLoading=true;
       this.store.dispatch(ResturantActions.fetchAllResturants({ request: request }));
-      // this.isLoading=false;
     }
-     this.isLoading = false;
+    if (
+      this.allRestuarants.length > 0 &&this.featuredResturants.length > 0 &&this.recommendedResturants.length > 0) 
+      this.isLoading = false; // Set loading to false after loading is complete
+    
   });
 
   }
